788215cc1d1475a976253572f71b8efcbd1cd2cd
Jacob Huang <65430628+Jachuang321@users.noreply.github.com>
2025-03-02 18:37:05 -0500

    feat(qpcr): added methods to get sample labels and create condition cols

--- dctap/qpcr/utils/core.py
+++ dctap/qpcr/utils/core.py
@@ -1,13 +1,13 @@
 import re
 import glob
-import numpy as np
 import pandas as pd
 from pathlib import Path
 from typing import cast
 
+from dctap.libs.pandas import display, displaydf_full
+
 from dctap.qpcr.constants import QPCRPATHS
 from dctap.qpcr.constants import PLATES
-from dctap.qpcr.constants.plates import Plate96
 from dctap.qpcr.libs import Incrementor
 
 
@@ -27,6 +27,7 @@ def _layout_to_annotation(
     create an annotation csv file.
     """
 
+    # Asserts that required layout files exists
     if code_file is None:
         code_pattern: str = str(
             data_dir / experiment_id / (experiment_id + "*code.csv")
@@ -100,6 +101,7 @@ def _layout_to_annotation(
             sample = ""
             primer = ""
 
+            # Sometimes wells can be empty or Cq values is N/A
             try:
                 sample_id: int = int(well) - code_offset
                 sample = code_map[sample_id]
@@ -108,6 +110,12 @@ def _layout_to_annotation(
                 sample = ""
                 primer = ""
 
+            """
+            Munson G. 96-well plate to 384-well plate layout
+            Each well on 96-well represents 4 neigbhoring wells on 384-well plate
+            E.g. 96-well plate A01 := the same wells A01, A02, B01, B02 on 384-well plate
+                 and so on.
+            """
             template_list[increment] = sample  # type: ignore
             template_list[increment + PLATES.P384.COL] = sample  # type: ignore
 
@@ -127,6 +135,7 @@ def _layout_to_annotation(
 
         increment.skip(PLATES.P384.COL)
 
+    # Write annotation file
     annotation_file = data_dir / experiment_id / (plate_id + "-annotation.csv")
     with open(annotation_file, "w") as f:
         f.write(",".join(["Well", "Sample", "Primer"]) + "\n")
@@ -136,9 +145,12 @@ def _layout_to_annotation(
                 + "\n"
             )
 
+    # return path to annotation file as list[str] consistent with glob query
     return [str(annotation_file)]
 
 
+# WARNING:
+# Depreciating soon: no longer setting conditions after getting relative Cq values
 def extract_key(s):
     m = re.match(r"^(.*)_TR\d+_(.*)$", s)
     if m:
@@ -157,6 +169,8 @@ def get_plate_data(
     Given a plate id, locate the annotations csv file and raw data csv files,
     then extract useful data, merge, and return the dataframe.
     """
+
+    # Locate annotation csv file. If it does not exist, create one
     annotation_pattern = str(
         data_dir / experiment_id / ("*" + plate_id + "*annotation.csv")
     )
@@ -167,11 +181,13 @@ def get_plate_data(
     assert len(annotation_files) == 1
     annotation_file = str(annotation_files[0])
 
+    # Locate and read data
     data_pattern = str(data_dir / experiment_id / ("*" + plate_id + ".csv"))
     data_files = glob.glob(data_pattern)
     assert len(data_files) == 1
     data_file = data_files[0]
 
+    # Merging data and annotations
     df = cast(pd.DataFrame, pd.read_csv(annotation_file))
     df_annotations = cast(pd.DataFrame, df[["Well", "Sample", "Primer"]])
 
@@ -188,6 +204,62 @@ def get_plate_data(
     return df
 
 
+def get_sample_metadata(sample: pd.Series | list[str], sep: str = "_") -> pd.DataFrame:
+    """
+    Given a list of strings and a separator, return a dataframe the string
+    split by separators into columns. The purpose for this method is to
+    help assign conditions.
+    """
+    # Convert to Series if provided a sample list
+    if sample is not pd.Series:
+        sample = pd.Series(sample)
+
+    # Get unique sample names and split metadata for modular labelling
+    sample = sample.copy().drop_duplicates().reset_index(drop=True)
+    metadata = sample.str.split(sep, expand=True)
+
+    first_col_index = 0
+    metadata.insert(
+        first_col_index,
+        "Sample",
+        sample,
+    )
+
+    return metadata
+
+
+def set_conditions(
+    df: pd.DataFrame,
+    sample_metadata: pd.DataFrame,
+    conditions: list[str],
+    merge_cols: list[str],
+) -> pd.DataFrame:
+    """
+    Creates condition columns for the df DataFrame based on the given conditions
+    list of list strings using the sample_metadata as a mapping.
+    """
+    sample_metadata = sample_metadata.copy()
+    metadata_colnames = sample_metadata.columns[1:].tolist()
+
+    # Create user defined condition columns
+    for i, condition in enumerate(conditions):
+        cols = list(merge_cols[i])
+        sample_metadata[condition] = sample_metadata[int(cols[0])].str.cat(
+            [sample_metadata[int(col)] for col in cols[1:]], sep="_"
+        )
+    #
+    # Drop sample_metadata columns
+    sample_metadata = sample_metadata.drop(metadata_colnames, axis="columns")
+
+    # Merge condition columns to main df
+    df = df.copy()
+    df = df.merge(sample_metadata, on="Sample", how="left")
+
+    return df
+
+
+# TODO:
+# This method is still in developement
 def get_deltaCq_expression_data(
     df,
     test_primer,
@@ -200,21 +272,26 @@ def get_deltaCq_expression_data(
     """
     for i in ["Well", "Sample", "Primer", "Cq", "experiment_id"]:
         assert i in df.columns
+    df = df.copy()
 
     # Get test and reference primers
-    df_ref = df[df.Primer == ref_primer].copy()
+    df_ref = df[df.Primer == ref_primer]
     df_ref.rename(columns={"Cq": "Cq_ref"}, inplace=True)
 
-    df_test = df[df.Primer == test_primer].copy()
+    df_test = df[df.Primer == test_primer]
     df_test.rename(columns={"Cq": "Cq_test"}, inplace=True)
 
     # Get average Cq values of technical replicates
     df_ref_mean = df_ref.groupby("Sample", as_index=False)[["Cq_ref"]].mean()
     df_test_mean = df_test.groupby("Sample", as_index=False)[["Cq_test"]].mean()
-    df = df_ref_mean.merge(df_test_mean, on="Sample")
 
-    # Calculate deltaCq
-    df["deltaCq"] = df.Cq_test - df.Cq_ref
+    # Merge results
+    df_results = df_ref_mean.merge(df_test_mean, on="Sample")
+    df_results["deltaCq"] = df_results.Cq_test - df_results.Cq_ref
+
+    print(df_results)
+
+    df = df.merge(df_results, on="Sample", how="right")
 
     return df
 
@@ -230,9 +307,17 @@ if __name__ == "__main__":
     df = pd.concat(dfs)
     df.reset_index(inplace=True, drop=True)
 
+    df_samples = get_sample_metadata(cast(pd.Series, df["Sample"]))
+    # with displaydf_full():
+    #     display(df_sample)
+
     # Adding a few helpful columns
+    df = set_conditions(df, df_samples, ["bio_reps", "cond_sd"], ["012", "02"])
     df["group"] = [df.Sample[i] + "___" + df.Primer[i] for i in range(len(df))]
     df["well_id"] = [df.plate_id[i] + "_" + df.Well[i] for i in range(len(df))]
     df["relExp_25"] = [2 ** (25 - df.Cq[i]) for i in range(len(df))]
 
-    df1 = get_deltaCq_expression_data(df, test_primer="CER1", ref_primer="GAPDH")
+    df.to_csv("~/Downloads/20250302-dftesting.csv")
+
+    # df1 = get_deltaCq_expression_data(df, test_primer="CER1", ref_primer="GAPDH")
+    # df1.to_csv("~/Downloads/20250302-df1testing.csv")
